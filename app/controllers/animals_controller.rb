bdcac436430f2699e87b3ed594260f3317f91608
EddieCarrion.github.io <ecar25L@gmail.com>
2023-04-21 11:31:06 -0700

    Progress

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -10,7 +10,25 @@ class AnimalsController < ApplicationController
     end
     def create
         animal = Animal.create(animal_params)
-        if animals.valid?
+        if animal.valid?
+            render json: animal
+        else render json: animal.errors
+        end
+    end
+
+    def destroy
+        animal = Animal.find(params[:id])
+        if animal.destroy
+            render json:animal
+        else 
+            render json: student.errors
+        end
+    end
+
+    def update
+        animal = Animal.find(params[:id])
+        animal.update(animal_params)
+        if animal.valid?
             render json: animal
         else render json: animal.errors
         end
